f805ff5a331375138d507f37b949ce12391ad1b5
Narendra Ram <email@narenkram.com>
2024-11-24 11:28:25 +0530

    add few more

--- src/constants.ts
+++ src/constants.ts
@@ -16,5 +16,20 @@ export const UNDERLYING_TOKEN_MAP: UnderlyingTokenMap = {
     Shoonya: "26037",
     Zerodha: "1020",
   },
+  MIDCPNIFTY: {
+    Flattrade: "26074",
+    Shoonya: "26074",
+    Zerodha: "1125",
+  },
+  SENSEX: {
+    Flattrade: "1",
+    Shoonya: "1",
+    Zerodha: "1120",
+  },
+  BANKEX: {
+    Flattrade: "12",
+    Shoonya: "12",
+    Zerodha: "1072",
+  },
   // Add more symbols as needed
 };
